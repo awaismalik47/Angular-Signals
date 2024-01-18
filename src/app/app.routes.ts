5da4027b7311a844d528264aa45c614cb8931712
Awais <awais@Hassans-Mac-mini-2.local>
2024-01-18 15:13:28 +0500

    async search bar with rxjs operators to handle the serach without clicking the search button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
+    {path:'',component:AdminComponent},
     { path: 'dashboard', component: DashboardComponent },
     {path:'user',component:UserComponent},
     {path:'admin',component:AdminComponent}
